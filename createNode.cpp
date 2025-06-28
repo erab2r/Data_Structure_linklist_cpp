a1b739ef9ac3477f5ccc9b4208e1fa0a7aea7a6a
erab2r <shkera213@gmail.com>
2025-06-29 00:23:38 +0600

    bn

--- createNode.cpp
+++ createNode.cpp
@@ -5,15 +5,15 @@ class Node{
     public:
     int val;
     Node*next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
 };
 int main(){
-    Node a,b,c;
-    a.val = 10;
-    b.val = 30;
-    c.val = 50;
+    Node a(10),b(30),c(60);
     a.next = &b;
     b.next = &c;
-    c.next = NULL;
     cout << a.val <<endl;
     cout <<a.next->val <<endl;
     cout << a.next->next->val << endl;
