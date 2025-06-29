cc0ddd29643c6377afcc613d0d070635b6ac7880
erab2r <shkera213@gmail.com>
2025-06-29 21:10:45 +0600

    ;

--- Dynamic_node.cpp
+++ Dynamic_node.cpp
@@ -11,6 +11,18 @@ class Node{
     }
 };
 int main(){
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(40);
+
+    head->next = a;
+    a->next = b;
+
+    cout << head->val <<endl;
+    cout << head->next->val <<endl;
+    cout << head->next->next->val <<endl;
+
+    
     
     
 }
